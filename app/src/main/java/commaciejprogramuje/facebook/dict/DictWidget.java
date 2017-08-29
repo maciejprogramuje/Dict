b2f40d05065f6c0d676a34fd3d727dafed5322e7
maciejprogramuje <maciejsz1979@gmail.com>
2017-08-29 23:21:06 +0200

    ok

--- app/src/main/java/commaciejprogramuje/facebook/dict/DictWidget.java
+++ app/src/main/java/commaciejprogramuje/facebook/dict/DictWidget.java
@@ -5,7 +5,9 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -15,12 +17,16 @@ import commaciejprogramuje.facebook.dict.R;
  * Implementation of App Widget functionality.
  */
 public class DictWidget extends AppWidgetProvider {
+    String number2 = "start";
+    private static final String MyOnClickA = "myOnClickA";
+    private static final String MyOnClickB = "myOnClickB";
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
-            String number = String.format("%03d", (new Random().nextInt(900) + 100));
+            String number = String.valueOf(new Random().nextInt(100));
 
             RemoteViews remoteViews = new RemoteViews(context.getPackageName(),R.layout.dict_widget);
             remoteViews.setTextViewText(R.id.appwidget_text, number);
@@ -28,8 +34,12 @@ public class DictWidget extends AppWidgetProvider {
             Intent intent = new Intent(context, DictWidget.class);
             intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
             intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+
             PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-            remoteViews.setOnClickPendingIntent(R.id.actionButton, pendingIntent);
+
+            remoteViews.setOnClickPendingIntent(R.id.buttonA, getPendingSelfIntent(context, MyOnClickA));
+            remoteViews.setOnClickPendingIntent(R.id.buttonB, getPendingSelfIntent(context, MyOnClickB));
+
             appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
         }
     }
@@ -42,6 +52,23 @@ public class DictWidget extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        if (MyOnClickA.equals(intent.getAction())){
+            number2 = "a";
+        } else if (MyOnClickB.equals(intent.getAction())) {
+            number2 = "b";
+        }
+        Toast.makeText(context, number2, Toast.LENGTH_SHORT).show();
+    }
+
+    protected PendingIntent getPendingSelfIntent(Context context, String action) {
+        Intent intent = new Intent(context, getClass());
+        intent.setAction(action);
+        return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 }
 
