9edb1cd8ecd17b7db2ec23a82f014f9d0c28e4f9
maciejprogramuje <maciejsz1979@gmail.com>
2017-08-29 20:57:25 +0200

    Initial commit

---
+++ app/src/main/java/commaciejprogramuje/facebook/dict/DictWidget.java
@@ -0,0 +1,47 @@
+package commaciejprogramuje.facebook.dict;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import java.util.Random;
+
+import commaciejprogramuje.facebook.dict.R;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class DictWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            String number = String.format("%03d", (new Random().nextInt(900) + 100));
+
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),R.layout.dict_widget);
+            remoteViews.setTextViewText(R.id.appwidget_text, number);
+
+            Intent intent = new Intent(context, DictWidget.class);
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setOnClickPendingIntent(R.id.actionButton, pendingIntent);
+            appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
